09a084273a0dc90d1c314a6526075f33c8ed1e37
andwow <53525076+andwow@users.noreply.github.com>
2021-06-28 01:47:18 +0300

    created product class

---
+++ app/src/main/java/com/example/petshopproject/models/Product.java
@@ -0,0 +1,38 @@
+package com.example.petshopproject.models;
+
+public class Product {
+    private int productId;
+    private String name;
+    private String description;
+    private float price;
+
+    public Product(int productId, String name, float price) {
+        this.productId = productId;
+        this.name = name;
+        this.price = price;
+    }
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+    public int getProductId() {
+        return productId;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setPrice(float price) {
+        this.price = price;
+    }
+    public float getPrice() {
+        return price;
+    }
+}
